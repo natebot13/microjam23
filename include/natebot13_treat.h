0b31fef6949df6b8bd6e3681ea986cb067193f0c
Nathan Pannell <tekkbot@gmail.com>
2023-10-04 02:34:34 -0700

    MVP of treat minigame

--- include/natebot13_treat.h
+++ include/natebot13_treat.h
@@ -2,20 +2,43 @@
 
 #include "mj/mj_game.h"
 
-#include "bn_sprite_ptr.h"
-#include "bn_sprite_items_natebot13_spook_ghost.h"
+#include "bn_regular_bg_ptr.h"
+#include "bn_regular_bg_items_natebot13_treat_bowl.h"
+#include "bn_regular_bg_items_natebot13_treat_hand_open.h"
+#include "bn_regular_bg_items_natebot13_treat_hand_close.h"
+
+#include "bn_fixed_rect.h"
+#include "bn_fixed.h"
 
 namespace natebot13_treat {
 
-class hand {
-  bn::sprite_ptr _sprite = bn::sprite_items::natebot13_spook_ghost.create_sprite(0, 0);
+class candy {
+ public:
+  candy();
+  void move(bn::fixed_point amount) { _bg.set_position(_bg.position() + amount); }
+  bn::fixed_rect get_rect();
+  bool grabbed = false;
+ private:
+  bn::regular_bg_ptr _bg;
 };
 
-class treat : mj::game {
+class hand {
+ public:
+  hand(candy& candy);
+  bool closed = false;
+  void update();
+  bn::fixed_rect get_rect();
+  bn::fixed speed = 3;
+ private:
+  bn::regular_bg_ptr _bg;
+  candy& _candy;
+};
+
+class treat : public mj::game {
  public:
   treat(int completed_games, const mj::game_data& data);
 
-  [[nodiscard]] bn::string<16> title() const final { return "Spook!"; }
+  [[nodiscard]] bn::string<16> title() const final { return "Treat!"; }
 
   [[nodiscard]] int total_frames() const final { return _total_frames; }
 
@@ -27,9 +50,14 @@ class treat : mj::game {
 
   void fade_out(const mj::game_data& data) final;
 
+  bn::fixed_rect get_rect();
+
  private:
   int _total_frames;
   bool _victory = false;
+  bn::regular_bg_ptr _bg = bn::regular_bg_items::natebot13_treat_bowl.create_bg(0, 0);
+  candy _candy;
+  hand _hand{_candy};
 };
 
 }  // namespace natebot13_treat
--- src/natebot13_treat.cpp
+++ src/natebot13_treat.cpp
@@ -1,10 +1,97 @@
 #include "natebot13_treat.h"
 
+#include "mj/mj_game_list.h"
+
+#include "bn_keypad.h"
+#include "bn_math.h"
+
+#include "bn_regular_bg_items_natebot13_treat_candy.h"
+#include "bn_regular_bg_items_natebot13_treat_hand_close.h"
+#include "bn_regular_bg_items_natebot13_treat_hand_open.h"
+
+MJ_GAME_LIST_ADD(natebot13_treat::treat)
+
 namespace natebot13_treat {
 
-treat::treat(int completed_games, const mj::game_data& data) {
-  _total_frames = 5 * 60;
-  bool take = data.random.get_fixed(1) > .5;
+int mod(int i, int n) { return (i % n + n) % n; }
+
+hand::hand(candy &candy)
+    : _bg{bn::regular_bg_items::natebot13_treat_hand_open.create_bg(0, 0)},
+      _candy{candy} {}
+
+void hand::update() {
+  closed = bn::keypad::a_held();
+  if (bn::keypad::a_pressed()) {
+    _bg = bn::regular_bg_items::natebot13_treat_hand_close.create_bg(
+        _bg.position());
+    if (_candy.get_rect().intersects(get_rect())) {
+      _candy.grabbed = true;
+    }
+  }
+
+  if (bn::keypad::a_released()) {
+    _bg = bn::regular_bg_items::natebot13_treat_hand_open.create_bg(
+        _bg.position());
+    _candy.grabbed = false;
+  }
+
+  bn::fixed_point move;
+  move.set_x(bn::keypad::right_held() - bn::keypad::left_held());
+  move.set_y(bn::keypad::down_held() - bn::keypad::up_held());
+  _bg.set_position(_bg.position() + move * speed);
+
+  if (_candy.grabbed) {
+    _candy.move(move * speed);
+  }
 }
 
+bn::fixed_rect hand::get_rect() {
+  bn::fixed_point offset = _bg.position() + bn::fixed_point{128, 128 + 64};
+  bn::fixed_point p{mod(offset.x().integer(), 256),
+                    mod(offset.y().integer(), 256)};
+  return bn::fixed_rect(p, bn::size{16 + 24, 16});
 }
+
+candy::candy()
+    : _bg{bn::regular_bg_items::natebot13_treat_candy.create_bg(0, 0)} {}
+
+bn::fixed_rect candy::get_rect() {
+  bn::fixed_point offset = _bg.position() + bn::fixed_point{128, 128 - 73};
+  bn::fixed_point p{mod(offset.x().integer(), 256),
+                    mod(offset.y().integer(), 256)};
+  return bn::fixed_rect(p, bn::size{24, 16});
+}
+
+treat::treat([[maybe_unused]] int completed_games,
+             [[maybe_unused]] const mj::game_data &data) {
+  _total_frames = 10 * 60;
+  bool take = data.random.get_fixed(1) > .5;
+  if (take) {
+    _bg.set_y(_bg.y() + 142);
+  }
+}
+
+bn::fixed_rect treat::get_rect() {
+  bn::fixed_point offset = _bg.position() + bn::fixed_point{128, 128 + 64};
+  bn::fixed_point p{mod(offset.x().integer(), 256),
+                    mod(offset.y().integer(), 256)};
+  return bn::fixed_rect(p, bn::size{64, 64});
+}
+
+mj::game_result treat::play([[maybe_unused]] const mj::game_data &data) {
+  mj::game_result result;
+  _hand.update();
+
+  if (!_candy.grabbed && get_rect().intersects(_candy.get_rect())) {
+    _victory = true;
+  } else {
+    _victory = false;
+  }
+  return result;
+}
+
+void treat::fade_in([[maybe_unused]] const mj::game_data &data) {}
+
+void treat::fade_out([[maybe_unused]] const mj::game_data &data) {}
+
+} // namespace natebot13_treat
